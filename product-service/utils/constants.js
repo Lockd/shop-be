558d164c71405fe331f342f2f6776f9f7a03fbd1
Lockd <pikagrigoriy@gmail.com>
2022-07-16 23:39:23 +0300

    fixed typos

--- product-service/utils/constants.js
+++ product-service/utils/constants.js
@@ -1,7 +1,7 @@
 export const PRODUCT_LIST = [
   {
     count: 1,
-    description: "Handmade claypot covered in blue glossy glaze ",
+    description: "Handmade clay pot covered in blue glossy glaze",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
     priceUSD: 17,
     title: "Clay pot",
@@ -16,7 +16,7 @@ export const PRODUCT_LIST = [
   {
     count: 1,
     description:
-      "Huge bowl made of pot with volume of 1.5 liters. Eating out of this vessel will definately help you gain weight",
+      "Huge bowl made of clay with volume of 1.5 liters. Eating out of this vessel will definately help you gain weight",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a2",
     priceUSD: 32,
     title: "Clay bowl ha-ha u fat",
